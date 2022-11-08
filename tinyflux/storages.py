6c799086b0263e5acf086ad1b5dd56b59c41c981
Aleksander <60496858+AlKorochkin@users.noreply.github.com>
2022-11-08 21:19:00 +0300

    issue 17: disable universal newlines translation for CSV Storage
    * disable universal newlines translation by default for the CSVStorage instance
    
    Co-authored-by: Justin Fung <citrusvanilla@gmail.com>

--- tinyflux/storages.py
+++ tinyflux/storages.py
@@ -188,6 +188,7 @@ class CSVStorage(Storage):
         encoding: Optional[str] = None,
         access_mode: str = "r+",
         flush_on_insert: bool = True,
+        newline: Optional[str] = "",
         **kwargs,
     ) -> None:
         """Init a CSVStorage instance.
@@ -201,6 +202,7 @@ class CSVStorage(Storage):
             create_dirs: Create parent subdirectories.
             encoding: File encoding.
             access_mode: File access mode.
+            newline: Determines how to parse newline characters from the stream
         """
         super().__init__()
         self._encoding = encoding
@@ -216,7 +218,9 @@ class CSVStorage(Storage):
             create_file(path, create_dirs=create_dirs)
 
         # Open the file for reading/writing
-        self._handle = open(path, mode=self._mode, encoding=encoding)
+        self._handle = open(
+            path, mode=self._mode, encoding=encoding, newline=newline
+        )
 
         # Open a tempfile.
         self._temp_handle: Optional[Any] = None
